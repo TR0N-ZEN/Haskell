702ab62718939f5ee6c0e14945c1119a11f310be
TR0N-ZEN <theo.reichert7@gmail.com>
2021-08-06 23:12:57 +0200

    explaining usage of foldr

--- exercises/list_contains.hs
+++ exercises/list_contains.hs
@@ -4,6 +4,7 @@ b1 || b2
  | b2 = True
  | otherwise = False
 
+
 listContains :: Int -> [Int] -> Bool
 listContains x [] = False
 listContains x (y:ys) = (x == y) Main.|| listContains x ys
@@ -19,3 +20,20 @@ listContains x (y:ys) = (x == y) Main.|| listContains x ys
 	2. x == y
 	3. (x == y) Main.|| (listContains x ys))
 -}
+
+listContains' :: Int -> [Int] -> Bool
+listContains' x [] = False
+listContains' x ys = foldr (\ y r -> (y==x) Main.|| r) False ys
+
+-- since definition of foldr is
+--
+-- foldr :: (Int -> Int -> Int) -> Int -> [Int] -> Int
+-- foldr _ z []        = z
+-- foldr f z (x : xs)  = f x (foldr f z xs)
+--
+-- meaning that
+-- foldr (\ y r -> (y==x) Main.|| r) False (a:as)
+-- is equal to
+-- (a==x) Main.|| foldr (\ y r -> (y==x) Main.|| r) False as
+-- and so on
+-- seeing it like this makes the right associativity of haskell seem natural
